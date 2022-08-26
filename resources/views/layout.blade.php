be155eed7b4112dbfa1286b9e06dd0d558ed224d
danijcc <danicolmenares7@gmail.com>
2022-08-26 01:28:58 -0500

    nav resaltado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,12 @@
     @section(<nombre especificado>) y al finalizar @endsection dentro de la misma 
     escribiremos el contenido unico de la web--}}
     <title> @yield('title', 'Aprendible')</title>
+    <style>
+       .active a {
+        color: red;
+        text-decoration: none;
+       } 
+    </style>
 </head>
 <body>
     {{-- En el archivo layout va el menu de navegacion entre cualquier otro 
@@ -13,11 +19,22 @@
     posteriormente para implementar esto en alguna otra pagina o vista usamos
     la directiva de blade @extends('layout')  --}}
     <nav>
+        {{-- con un request podemos ver un previo de lo que esta pasando detras 
+        de todo es similar a un var dump
+        <pre>{{request()}}</pre> tex plano
+        <pre>{{dump(request())}}</pre> format json
+        <pre>{{request()->url()}}</pre> nos muestra la url completa
+        <pre>{{request()->path()}}</pre> nos muestra la url interna
+        <pre>{{dump(request()->routeIs('home'))}}</pre> nos permite comparar la ruta
+        --}}
+        
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
+            {{-- mediante un operador ternario ? evaluamos la expresion para activar los link  --}}
+            <li class="{{request()->routeIs('home') ? 'active' : ''}}"><a href="/">Home</a></li>
+            <li class="{{request()->routeIs('about') ? 'active' : ''}}"><a href="/about">About</a></li>
+            <li class="{{request()->routeIs('portfolio') ? 'active' : ''}}"><a href="/portfolio">Portfolio</a></li>
+            <li class="{{request()->routeIs('contact') ? 'active' : ''}}"><a href="/contact">Contact</a></li>
+           
         </ul>
     </nav>
     {{-- para agregar contenido dinamico usamos la directiva @yield('') podemos 
--- routes/web.php
+++ routes/web.php
@@ -118,4 +118,4 @@ Route::get('/portfolio','PortfolioController@index')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
 //Route::resource('projects','PortfolioCotroller');
 
-//video 13
+//video 13-04:26
